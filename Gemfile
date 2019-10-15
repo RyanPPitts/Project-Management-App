5eea76511c4d600fd016930c3d45d6389fe986c3
RyanPPitts <47765262+RyanPPitts@users.noreply.github.com>
2019-10-14 18:44:45 -0700

    trying to fix errors with user

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bulma-rails', '~> 0.6.1'
 gem 'simple_form', '~> 3.5'
-gem 'devise', '~> 4.4'
+gem 'devise'
 gem 'gravatar_image_tag', '~> 1.2'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem "wysiwyg-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
 
   resources :projects
   resources :teams
- as :user do
-  put '/user/confirmation' => 'confirmations#update', :via => :path, :as => :update_user_confirmation
- end
 
-  devise_for :users, controllers: { 
-    registrations: 'registrations', 
-    confirmation: 'confirmations'
+ 	as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
+
+   devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmations: 'confirmations'
   }
-  root 'home#index'
   
+  root 'home#index'
 end
