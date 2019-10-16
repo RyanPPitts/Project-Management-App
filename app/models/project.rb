8b54e97d89657a903a7919d5d781e0e9d3e09afb
RyanPPitts <ryanpitts26@gmail.com>
2019-10-16 01:32:44 -0700

    fixing the ruby project on new mac

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    belongs_to :Team
+    belongs_to :team
     belongs_to :user
 
     accepts_nested_attributes_for :team
--- db/schema.rb
+++ db/schema.rb
@@ -55,12 +55,13 @@ ActiveRecord::Schema.define(version: 20191014004739) do
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "last_sign_in_atr"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_atrails"
+    t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
